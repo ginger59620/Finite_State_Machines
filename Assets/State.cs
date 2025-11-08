57c2a9f7a0f34d1e1ee226461c69fe658e755ba3
ginger59620 <a59620@alunos.ipb.pt>
2025-11-08 15:01:31 +0000

    Finite state machine challage

--- Assets/State.cs
+++ Assets/State.cs
@@ -9,7 +9,7 @@ public class State
 {
    public enum STATE
     {
-        IDLE, PATROL, PURSUE, ATTACK, SLEEP
+        IDLE, PATROL, PURSUE, ATTACK, SLEEP, RUNAWAY
     };
     public enum EVENT
     {
@@ -58,6 +58,17 @@ public class State
         float angle = Vector3.Angle(direction, npc.transform.forward);
 
         if(direction.magnitude < visDist && angle < visAngle)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsPlayerBehind()
+    {
+        Vector3 direction = npc.transform.position - player.position;
+        float angle = Vector3.Angle(direction, npc.transform.forward);
+        if(direction.magnitude < 2 && angle < 30)
         {
             return true;
         }
@@ -150,6 +161,11 @@ public class Patrol : State
             nextState = new Pursue(npc, agent, anim, player);
             stage = EVENT.EXIT;
         }
+        else if(IsPlayerBehind())
+        {
+            nextState = new RunAway(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
     }
     public override void Exit()
     {
@@ -235,4 +251,36 @@ public class Attack : State
         shoot.Stop();
         base.Exit();
     }
+}
+
+public class RunAway : State
+{
+    GameObject safeLocation;
+    public RunAway(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.RUNAWAY;
+        safeLocation = GameObject.FindGameObjectWithTag("Safe");
+    }
+    public override void Enter()
+    {
+        anim.SetTrigger("isRunning");
+        agent.isStopped = false;
+        agent.speed = 6;
+        agent.SetDestination(safeLocation.transform.position);
+        base.Enter();
+    }
+    public override void Update()
+    {
+        if(agent.remainingDistance < 1)
+        {
+            nextState = new Idle(npc, agent, anim, player);
+            stage = EVENT.EXIT;    
+        }
+    }
+    public override void Exit()
+    {
+        anim.ResetTrigger("isRunning");
+        base.Exit();
+    }
+
 }
