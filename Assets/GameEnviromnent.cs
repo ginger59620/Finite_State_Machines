0b605fe91b2aaf98af62665911cc56742235026b
ginger59620 <a59620@alunos.ipb.pt>
2025-11-08 13:23:49 +0000

    Creating and using a state class

---
+++ Assets/GameEnviromnent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnviromnent
+{
+    public static GameEnviromnent instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+   public static GameEnviromnent Singleton
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameEnviromnent();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State 
+{
+   public enum STATE
+    {
+        IDLE, PATROL, PERSUE, ATTACK, SLEEP
+    };
+    public enum EVENT
+    {
+        ENTER, UPDATE, EXIT
+    };
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+    protected NavMeshAgent agent;
+
+    float visDist = 10.0f;
+    float visAngle = 30.0f;
+    float shootDist = 7.0f;
+
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
+
+    public virtual void Enter() { stage = EVENT.UPDATE; }
+    public virtual void Update() { stage = EVENT.UPDATE; }
+    public virtual void Exit() { stage = EVENT.EXIT; }
+
+    public State Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;
+        }
+        return this;
+    }
+
+}
